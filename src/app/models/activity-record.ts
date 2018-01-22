1ddb8e11c72dcaafba339df0652cac8cb6533d48
Alex Aylwin <alex.aylwin@rbc.com>
2018-01-22 09:45:17 -0500

    Linting fixes, added a tasks/launch for debugging

--- src/app/models/activity-record.ts
+++ src/app/models/activity-record.ts
@@ -1,22 +1,22 @@
 export class ActivityRecord {
-	startTime: Date;
-	endTime: Date;
-	duration: number;
-	activityId: number;
+  startTime: Date;
+  endTime: Date;
+  duration: number;
+  activityId: number;
 
 	/**
 	 * Deserialize a JSON object into an AcivityRecord
 	 * @param obj the JSON object to deseralize
 	 */
-	public static deserialize(obj: ßany): ActivityRecord {
-		let activityRecord:ActivityRecord;
-		//We won't allow partial deserialization, we must have all fields
+  public static deserialize(obj: any): ActivityRecord {
+    const activityRecord: ActivityRecord = new ActivityRecord();
+		// We won't allow partial deserialization, we must have all fields
 
-		if(obj.activityId === undefined || !obj.activityId
-		ß|| obj.endTime === undefined || !obj.endTime
+    if (obj.activityId === undefined || !obj.activityId
+      || obj.endTime === undefined || !obj.endTime
       || obj.startTime === undefined || !obj.startTime
-      || obj.duration === undefined || !obj.duration 
-      ) {
+      || obj.duration === undefined || !obj.duration
+    ) {
         throw new TypeError('Missing field when trying to deserialize ActivityRecord: ' + JSON.stringify(obj));
     }
     activityRecord.endTime = obj.endTime;
