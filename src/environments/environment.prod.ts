d5fed27e25de18df44ca088cda7ab4320d54d54a
Alex <alex.aylwin@rbc.com>
2018-05-12 21:14:01 -0400

    Added cloud configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const SERVICE_BASE_URL:String = "http://localhost:8080/tracker/api";
+export const SERVICE_BASE_URL:String = "http://tracker-service.alexaylwin.com/tracker/api";
