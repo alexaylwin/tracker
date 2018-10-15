c5206988e09308ea7ac114b70ec21c3632b0e14b
Alex <AlexAylwin@gmail.com>
2018-10-15 08:50:28 -0400

    Added component for resuming an in-progress activity

---
+++ src/app/components/resume-activity/resume-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'resume-activity',
+	templateUrl: './resume-activity.component.html'
+})
+export class ResumeActivityComponent implements OnInit {
+
+	ngOnInit(): void {}
+}
