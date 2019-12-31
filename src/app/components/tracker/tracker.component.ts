7f4e055e106adeb85df4f061225d88ffdb9cdbd3
Alex <AlexAylwin@gmail.com>
2019-12-31 07:32:56 -0500

    Added the stateservice to the tracker component

--- src/app/components/tracker/tracker.component.ts
+++ src/app/components/tracker/tracker.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { RecentActivitiesComponent } from '../recent-activities/recent-activities.component';
 import { Observable, Subject } from 'rxjs/Rx';
 import { ActivityRecord } from '../../models/activity-record';
+import { StateService } from 'app/services/state.service';
 
 @Component({
   selector: 'app-tracker',
@@ -12,13 +13,16 @@ export class TrackerComponent implements OnInit {
   @ViewChild(RecentActivitiesComponent)
   private recentActivitiesList: RecentActivitiesComponent;
 
+  constructor(private stateService: StateService) { }
+
+  ngOnInit() {
+    //this.stateService.
+  }
+
   onTimerStopped(event: any) {
     this.recentActivitiesList.addNewRecord(event);
   }
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
